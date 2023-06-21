a0a5b128189c033757ea6c3695d4c412583f235a
Lean Mendoza <leandro@decentraland.org>
2023-06-21 10:55:34 -0300

    fix rs format

--- rust/decentraland-godot-lib/src/scene_runner/components/mod.rs
+++ rust/decentraland-godot-lib/src/scene_runner/components/mod.rs
@@ -1,3 +1,4 @@
+pub mod billboard;
 pub mod gltf_container;
 pub mod material;
 pub mod mesh_collider;
@@ -5,4 +6,3 @@ pub mod mesh_renderer;
 pub mod pointer_events;
 pub mod text_shape;
 pub mod transform_and_parent;
-pub mod billboard;
--- rust/decentraland-godot-lib/src/scene_runner/input.rs
+++ rust/decentraland-godot-lib/src/scene_runner/input.rs
@@ -48,6 +48,4 @@ impl InputState {
         }
         result
     }
-
-    
 }
