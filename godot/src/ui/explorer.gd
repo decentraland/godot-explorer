84729e29428ce5af0a34eb06cf009b1015de6b4a
Lean Mendoza <leandro@decentraland.org>
2023-06-29 13:11:53 -0300

    fix debug raycast

--- godot/src/ui/explorer.gd
+++ godot/src/ui/explorer.gd
@@ -37,9 +37,9 @@ func _process(_dt):
 
 
 func _ready():
-	var parcel_position: Vector2i = Vector2i(72, -4)
-	player.position = 16 * Vector3(parcel_position.x, 0.1, -parcel_position.y)
-	player.look_at(16 * Vector3(parcel_position.x + 1, 0, -(parcel_position.y + 1)))
+	var start_parcel_position: Vector2i = Vector2i(74, -2)
+	player.position = 16 * Vector3(start_parcel_position.x, 0.1, -start_parcel_position.y)
+	player.look_at(16 * Vector3(start_parcel_position.x + 1, 0, -(start_parcel_position.y + 1)))
 
 	scene_runner = get_tree().root.get_node("scene_runner")
 	scene_runner.set_camera_and_player_node(player.camera, player, self._on_scene_console_message)
