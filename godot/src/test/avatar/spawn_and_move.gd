5490e73d9d847d18d51f27e649a23e362ac2eee3
Lean Mendoza <leandro@decentraland.org>
2023-09-16 11:34:56 -0300

    fix format

--- godot/src/test/avatar/spawn_and_move.gd
+++ godot/src/test/avatar/spawn_and_move.gd
@@ -1,18 +1,19 @@
 extends Node
 class_name TestSpawnAndMoveAvatars
 
-
 var test_avatar_N: int = 100
 var moving_avatars: bool = false
 var moving_t: float = 0
-var spawning_avatars:bool = false
-var spawning_i:int = 0
-var spawning_position:Array[Vector3] = []
+var spawning_avatars: bool = false
+var spawning_i: int = 0
+var spawning_position: Array[Vector3] = []
 
 var wearable_data = {}
 
+
 func set_wearable_data(_wearable_data):
 	wearable_data = _wearable_data
+
 
 func get_random_body():
 	var to_pick = []
@@ -22,6 +23,7 @@ func get_random_body():
 			to_pick.push_back(wearable_id)
 
 	return to_pick.pick_random()
+
 
 func get_random_wearable(category: String, body_shape_id: String):
 	var to_pick = []
@@ -33,37 +35,41 @@ func get_random_wearable(category: String, body_shape_id: String):
 
 	return to_pick.pick_random()
 
+
 func _process(dt):
 	var avatar_scene: AvatarScene = get_node("/root/avatars")
 	if spawning_avatars and spawning_i < test_avatar_N:
 		var body_shape_id = get_random_body()
-		var avatar_data = { 
-			"base_url": "https://peer.decentraland.org/content", 
-			"name": "Avatar#" + str(spawning_i), 
+		var avatar_data = {
+			"base_url": "https://peer.decentraland.org/content",
+			"name": "Avatar#" + str(spawning_i),
 			"body_shape": body_shape_id,
-			"eyes": Color(randf(), randf(), randf()), 
-			"hair": Color(randf(), randf(), randf()), 
-			"skin": Color(0.8, 0.6078, 0.4667, 1), 
-			"wearables": [
+			"eyes": Color(randf(), randf(), randf()),
+			"hair": Color(randf(), randf(), randf()),
+			"skin": Color(0.8, 0.6078, 0.4667, 1),
+			"wearables":
+			[
 				get_random_wearable(Wearables.Categories.MOUTH, body_shape_id),
 				get_random_wearable(Wearables.Categories.HAIR, body_shape_id),
 				get_random_wearable(Wearables.Categories.UPPER_BODY, body_shape_id),
 				get_random_wearable(Wearables.Categories.LOWER_BODY, body_shape_id),
 				get_random_wearable(Wearables.Categories.FEET, body_shape_id),
 				get_random_wearable(Wearables.Categories.EYES, body_shape_id),
-			], 
-			"emotes": [] 
+			],
+			"emotes": []
 		}
-		
-		var initial_position := Vector3(randf_range(-10, 10), 0.0, randf_range(-10, 10)).normalized()
+
+		var initial_position := (
+			Vector3(randf_range(-10, 10), 0.0, randf_range(-10, 10)).normalized()
+		)
 		var transform = Transform3D(Basis.IDENTITY, initial_position)
 		var alias = 10000 + spawning_i
 		avatar_scene.add_avatar(alias)
 		avatar_scene.update_avatar_profile(alias, avatar_data)
 		avatar_scene.update_avatar_transform(alias, transform)
-			
+
 		spawning_position.append(initial_position)
-		
+
 		spawning_i = spawning_i + 1
 		if spawning_i >= test_avatar_N:
 			spawning_avatars = false
@@ -75,16 +81,15 @@ func _process(dt):
 			var walk_delta = WALK_SPEED * moving_t
 			moving_t = 0
 			for i in range(spawning_position.size()):
-				var delta_position = Vector3(randf_range(-1, 1), 0.0, randf_range(-1, 1)).normalized() * walk_delta
+				var delta_position = (
+					Vector3(randf_range(-1, 1), 0.0, randf_range(-1, 1)).normalized() * walk_delta
+				)
 				var current_position = spawning_position[i]
 				var target_position = current_position + delta_position
 				var transform = Transform3D(Basis.IDENTITY, current_position)
 				transform = transform.looking_at(target_position)
 				transform.origin = target_position
-				
+
 				spawning_position[i] = target_position
 				var alias = 10000 + i
 				avatar_scene.update_avatar_transform(alias, transform)
-	
-				
-	
