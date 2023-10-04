95c8cfca05270d966e56ab6059cdab34865f1322
Mateo Miccino <mateomiccino@gmail.com>
2023-10-04 10:33:16 -0300

    fix: engine api with sdk6 deprecated apis, and add isServer (#54)

--- rust/decentraland-godot-lib/src/dcl/js/js_modules/EngineApi.js
+++ rust/decentraland-godot-lib/src/dcl/js/js_modules/EngineApi.js
@@ -7,14 +7,37 @@ module.exports.crdtSendToRenderer = async function (messages) {
     };
 }
 
-module.exports.sendBatch = async function () {
-    return { events: [] }
-}
-
 module.exports.crdtGetState = async function () {
     const data = (await Deno.core.opAsync("op_crdt_recv_from_renderer")).map((item) => new Uint8Array(item))
 
     return {
         data: data
     };
+}
+
+
+module.exports.isServer = async function() {
+    return {
+        isServer: false
+    }
+}
+
+/**
+ * @deprecated this is an SDK6 API.
+ * This function subscribe to an event from the renderer
+ */
+module.exports.subscribe = async function(message) {}
+
+/**
+ * @deprecated this is an SDK6 API.
+ * This function unsubscribe to an event from the renderer
+ */
+module.exports.unsubscribe = async function(message) {}
+
+/**
+ * @deprecated this is an SDK6 API.
+ * This function polls events from the renderer
+ */
+module.exports.sendBatch = async function() {
+    return { events: [] }
 }
