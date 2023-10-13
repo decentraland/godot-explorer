88ab41ebf160e0e4aa175a86646817315dfe4824
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-10-13 17:15:57 -0300

    improve force global scale component

--- godot/src/helpers_components/force_global_scale_component.gd
+++ godot/src/helpers_components/force_global_scale_component.gd
@@ -4,18 +4,7 @@ extends Node3D
 
 
 func _process(dt):
-	# Obtain the global scale
-	var current_global_scale = target.get_global_transform().basis.get_scale()
-
-	# If the global scale is not 1,1,1, adjust the local scale
-	if current_global_scale != Vector3(1, 1, 1):
-		# If the node has a parent, adjust the local scale based on
-		# the parent's global scale so that the resulting global scale
-		# is 1,1,1.
-		var parent = target.get_parent_node_3d()
-		if parent:
-			var parent_global_scale = parent.get_global_transform().basis.get_scale()
-			# Here we use the inverse of the parent's global scale
-			target.scale = target.scale / parent_global_scale
-		else:
-			target.scale = Vector3(1, 1, 1)
+	var global_scale = target.get_global_transform().basis.get_scale()
+	# If the global scale is not 1,1,1, adjust it
+	if global_scale != Vector3.ONE:
+		target.scale = target.scale / global_scale
