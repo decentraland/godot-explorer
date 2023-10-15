ed7f6808e944f806926f24334e061832d11edc9b
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-10-15 16:59:03 -0300

    fix overlapping bug, when the camera mode area is removed (scene unload), now it triggers the overlapping correctly

--- godot/src/logic/player/camera_mode_area_detector.gd
+++ godot/src/logic/player/camera_mode_area_detector.gd
@@ -3,23 +3,24 @@ extends Area3D
 signal block_camera_mode(mode: Global.CameraMode)
 signal unblock_camera_mode
 
+var overlapping_areas: Array[Area3D] = []
 
 func _on_area_entered(area):
 	if area is DclCameraModeArea3D:
+		overlapping_areas.push_back(area)
 		check_areas()
-
 
 func _on_area_exited(area):
 	if area is DclCameraModeArea3D:
+		overlapping_areas.erase(area)
 		check_areas()
 
 
 func get_last_dcl_camera_mode_area_3d() -> DclCameraModeArea3D:
-	return get_overlapping_areas().back()
-
+	return overlapping_areas.back()
 
 func check_areas():
-	if has_overlapping_areas():
+	if !overlapping_areas.is_empty():
 		var first_area = get_last_dcl_camera_mode_area_3d()  # get first
 		if first_area != null:
 			block_camera_mode.emit(first_area.forced_camera_mode)
