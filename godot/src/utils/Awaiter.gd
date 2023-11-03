f17773e6481779572ec1842c7f8c90f9bb1cede2
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-11-02 23:42:54 -0300

    fix gdformat

--- godot/src/utils/Awaiter.gd
+++ godot/src/utils/Awaiter.gd
@@ -1,18 +1,19 @@
 # util.gd
 class_name Awaiter
 
+
 class AllAwaiter:
 	var _mask: int
 	var _completed := false
 	var promise: Promise = Promise.new()
-	
+
 	func _init(funcs: Array) -> void:
 		var size := funcs.size()
 		assert(size < 64)
 		_mask = (1 << size) - 1
 		for i in size:
 			_call_func(i, funcs[i])
-	
+
 	func _call_func(i: int, f) -> void:
 		@warning_ignore("redundant_await")
 		if f is Promise:
@@ -26,6 +27,7 @@ class AllAwaiter:
 		if not _mask and not _completed:
 			_completed = true
 			promise.resolve()
+
 
 static func all(funcs: Array) -> void:
 	await AllAwaiter.new(funcs).promise.awaiter()
