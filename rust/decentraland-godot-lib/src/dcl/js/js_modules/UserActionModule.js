5ad1deddbcb9cb6d736fc794bd0793f37ccc90a3
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-11-06 13:33:21 -0300

    feat: add sdk6 request teleport

--- rust/decentraland-godot-lib/src/dcl/js/js_modules/UserActionModule.js
+++ rust/decentraland-godot-lib/src/dcl/js/js_modules/UserActionModule.js
@@ -1,1 +1,23 @@
-module.exports.requestTeleport = async function (body) { return {} }
+// @deprecated, only available for SDK6 compatibility. Use RestrictedActions/TeleportTo
+module.exports.requestTeleport = async function (body) {
+    const { destination } = req
+    if (destination === 'magic' || destination === 'crowd') {
+        return await Deno.core.ops.op_teleport_to([
+            0,
+            0,
+          ]);
+    } else if (!/^\-?\d+\,\-?\d+$/.test(destination)) {
+      return await Promise.reject(`teleportTo: invalid destination ${destination}`)
+    }
+
+    const coords = destination.split(',');
+
+    // Convert the separate parts to whole numbers.
+    let x = parseInt(coords[0], 10);
+    let y = parseInt(coords[1], 10);
+
+    return await Deno.core.ops.op_teleport_to([
+        x,
+        y,
+      ]);
+}
