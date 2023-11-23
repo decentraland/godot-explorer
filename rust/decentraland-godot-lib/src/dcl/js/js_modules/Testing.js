9c56121392de233e8bbacbfb2694ea9fba4aa33a
Lean Mendoza <leandro@decentraland.org>
2023-11-23 17:18:25 -0300

    fix promise

--- rust/decentraland-godot-lib/src/dcl/js/js_modules/Testing.js
+++ rust/decentraland-godot-lib/src/dcl/js/js_modules/Testing.js
@@ -11,10 +11,10 @@ function emptyTesting() {
 }
 
 function testingModule() {
-    async function takeAndCompareSnapshot(body) {
+    function takeAndCompareSnapshot(body) {
         const { id, cameraPosition, cameraTarget, snapshotFrameSize, tolerance } = body
 
-        return await Deno.core.ops.op_take_and_compare_snapshot(
+        return Deno.core.ops.op_take_and_compare_snapshot(
             id,
             [cameraPosition.x, cameraPosition.y, cameraPosition.z],
             [cameraTarget.x, cameraTarget.y, cameraTarget.z],
