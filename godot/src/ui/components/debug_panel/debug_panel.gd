75b2433fecfb9aed30690f7e9daf9155be457a5e
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-12-04 10:32:59 -0300

    fix onready and add scroll to the last (when doesn't have focus)

--- godot/src/ui/components/debug_panel/debug_panel.gd
+++ godot/src/ui/components/debug_panel/debug_panel.gd
@@ -7,6 +7,7 @@ enum SceneLogLevel {
 }
 
 const ICON_COLUMN_WIDTH = 20
+
 var icon_log: Texture2D = preload("res://src/ui/components/debug_panel/icons/Log.svg")
 var icon_error: Texture2D = preload("res://src/ui/components/debug_panel/icons/Error.svg")
 var icon_warning: Texture2D = preload("res://src/ui/components/debug_panel/icons/Warning.svg")
@@ -21,20 +22,25 @@ var icon_visible: Texture2D = preload(
 	"res://src/ui/components/debug_panel/icons/GuiVisibilityVisible.svg"
 )
 
+@onready var tree_console: Tree = %Tree_Console
+@onready var tab_container_debug_panel: TabContainer = %TabContainer_DebugPanel
+@onready var button_show_hide: Button = %Button_ShowHide
+@onready var popup_menu: PopupMenu = %PopupMenu
+
 
 func _ready():
-	%Tree_Console.set_column_expand(0, false)
-	%Tree_Console.set_column_custom_minimum_width(0, ICON_COLUMN_WIDTH)
+	tree_console.set_column_expand(0, false)
+	tree_console.set_column_custom_minimum_width(0, ICON_COLUMN_WIDTH)
 
-	%Tree_Console.set_column_expand(1, true)
-	%Tree_Console.set_column_clip_content(1, true)
+	tree_console.set_column_expand(1, true)
+	tree_console.set_column_clip_content(1, true)
 
-	%Tree_Console.create_item()  # root
+	tree_console.create_item()  # root
 
 
 func on_console_add(scene_title: String, level: int, _timestamp: float, text: String) -> void:
-	var root: TreeItem = %Tree_Console.get_root()
-	var item: TreeItem = %Tree_Console.create_item(root)
+	var root: TreeItem = tree_console.get_root()
+	var item: TreeItem = tree_console.create_item(root)
 	item.set_collapsed_recursive(true)
 
 	var color := Color.BLACK
@@ -65,25 +71,29 @@ func on_console_add(scene_title: String, level: int, _timestamp: float, text: St
 
 	# Check size of text...
 	var width = get_string_width(message)
-	var text_column_width = %Tree_Console.size.x - ICON_COLUMN_WIDTH - 24
+	var text_column_width = tree_console.size.x - ICON_COLUMN_WIDTH - 24
 	if text_column_width <= width:
 		var lines: Array[String] = word_wrap(message)
 		var index = 0
 		for line in lines:
 			index += 1
-			var subitem: TreeItem = %Tree_Console.create_item(item)
+			var subitem: TreeItem = tree_console.create_item(item)
 			subitem.set_text(0, "Line %d" % index)
 			subitem.set_text(1, line)
 
+	if not tree_console.has_focus():
+		# go to last
+		tree_console.scroll_to_item(item)
+
 
 func get_string_width(message: String) -> int:
-	var font: Font = %Tree_Console.get_theme_font("font")
-	var font_size = %Tree_Console.get_theme_font_size("font_size")
+	var font: Font = tree_console.get_theme_font("font")
+	var font_size = tree_console.get_theme_font_size("font_size")
 	return font.get_string_size(message, 0, -1, font_size).x
 
 
 func word_wrap(message: String) -> Array[String]:
-	var text_column_width = %Tree_Console.size.x - ICON_COLUMN_WIDTH - 24
+	var text_column_width = tree_console.size.x - ICON_COLUMN_WIDTH - 24
 	var words: PackedStringArray = message.split(" ")
 	var line_text_constructor: String = ""
 	var lines: Array[String] = []
@@ -107,39 +117,39 @@ func word_wrap(message: String) -> Array[String]:
 
 
 func _on_button_clear_pressed():
-	%Tree_Console.clear()
+	tree_console.clear()
 
 
 func _on_line_edit_filter_text_changed(new_text):
-	var children = %Tree_Console.get_root().get_children()
+	var children = tree_console.get_root().get_children()
 	for item: TreeItem in children:
 		var hide = item.get_text(1).find(new_text) == -1 and not new_text.is_empty()
 		item.visible = not hide
 
 
 func _on_button_show_hide_pressed():
-	%TabContainer_DebugPanel.visible = not %TabContainer_DebugPanel.visible
-	if %TabContainer_DebugPanel.visible:
-		%Button_ShowHide.icon = icon_visible
+	tab_container_debug_panel.visible = not tab_container_debug_panel.visible
+	if tab_container_debug_panel.visible:
+		button_show_hide.icon = icon_visible
 	else:
-		%Button_ShowHide.icon = icon_hidden
+		button_show_hide.icon = icon_hidden
 
 
 func _on_tree_console_item_mouse_selected(position, mouse_button_index):
 	if mouse_button_index != MOUSE_BUTTON_RIGHT:
 		return
 
-	%PopupMenu.clear(true)
-	%PopupMenu.reset_size()
+	popup_menu.clear(true)
+	popup_menu.reset_size()
 
-	if %Tree_Console.get_selected():
-		%PopupMenu.add_icon_item(icon_action_copy, "Copy ")
-		%PopupMenu.set_position(%Tree_Console.get_screen_position() + position)
-		%PopupMenu.popup()
+	if tree_console.get_selected():
+		popup_menu.add_icon_item(icon_action_copy, "Copy ")
+		popup_menu.set_position(tree_console.get_screen_position() + position)
+		popup_menu.popup()
 
 
 func _on_popup_menu_index_pressed(index):
 	# Only has copy
 	if index == 0:
-		var text = %Tree_Console.get_selected().get_text(1)
+		var text = tree_console.get_selected().get_text(1)
 		DisplayServer.clipboard_set(text)
