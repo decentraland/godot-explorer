d1ceac9aac7f9f984a47f7f62fac28a1e3fba832
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-12-11 11:06:28 -0300

    w

--- rust/decentraland-godot-lib/android-build.sh
+++ rust/decentraland-godot-lib/android-build.sh
@@ -2,34 +2,31 @@
 
 set -e
 
-ANDROID_NDK=~/Android/Sdk/ndk/25.2.9519653
 if [[ -z "${ANDROID_NDK}" ]]; then
     # Tested with NDK 25.2.9519653
     if [[ -z "${ANDROID_SDK}" ]]; then
-        CUR_ANDROID_NDK=$ANDROID_SDK/ndk/25.2.9519653
+        ANDROID_NDK=$ANDROID_SDK/ndk/25.2.9519653
     else
-        CUR_ANDROID_NDK=~/Android/Sdk/ndk/25.2.9519653
+        ANDROID_NDK=~/Android/Sdk/ndk/25.2.9519653
     fi
-else
-    CUR_ANDROID_NDK=$ANDROID_NDK
+    ANDROID_NDK_HOME=$ANDROID_NDK
 fi
 
 if [[ -z "${FFMPEG_DIR}" ]]; then
-    export FFMPEG_DIR=~/Documents/github/ffmpeg-kit/prebuilt/android-arm64/ffmpeg
+    FFMPEG_DIR=~/Documents/github/ffmpeg-kit/prebuilt/android-arm64/ffmpeg
 fi
 
 
 # Run the specified commands
-export TARGET_CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android33-clang
-export TARGET_CXX=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android33-clang++
-export TARGET_AR=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
-export RUSTY_V8_MIRROR=https://github.com/leanmendoza/rusty_v8/releases/download
-export CARGO_FFMPEG_SYS_DISABLE_SIZE_T_IS_USIZE=1
+TARGET_CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android33-clang
+TARGET_CXX=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android33-clang++
+TARGET_AR=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
+RUSTY_V8_MIRROR=https://github.com/leanmendoza/rusty_v8/releases/download
+CARGO_FFMPEG_SYS_DISABLE_SIZE_T_IS_USIZE=1
+CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android33-clang
 
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android33-clang
 
-
-ANDROID_NDK_HOME=$ANDROID_NDK GN_ARGS=use_custom_libcxx=false RUST_BACKTRACE=full cargo build --target aarch64-linux-android -vv --verbose --release
+GN_ARGS=use_custom_libcxx=false RUST_BACKTRACE=full cargo build --target aarch64-linux-android -vv --verbose --release
 
 mkdir -p ../../godot/lib/android/
 cp target/aarch64-linux-android/release/libdecentraland_godot_lib.so ../../godot/lib/android/libdecentraland_godot_lib.so
