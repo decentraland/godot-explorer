3ed6d69cb7c06805b77b81d192b32d06f7e5f6d9
Lean Mendoza <leandro@decentraland.org>
2023-12-27 14:14:05 -0300

    fix snapshot folder

--- rust/xtask/src/main.rs
+++ rust/xtask/src/main.rs
@@ -161,6 +161,12 @@ fn main() -> Result<(), anyhow::Error> {
 }
 
 pub fn coverage_with_itest(devmode: bool) -> Result<(), anyhow::Error> {
+    remove_dir("../../snapshots")?;
+    create_dir_all("../../snapshots")?;
+
+    let snapshot_folder = Path::new("../../snapshots");
+    let snapshot_folder = snapshot_folder.canonicalize()?;
+
     remove_dir("../coverage")?;
     create_dir_all("../coverage")?;
 
@@ -192,9 +198,6 @@ pub fn coverage_with_itest(devmode: bool) -> Result<(), anyhow::Error> {
         Some(build_envs.clone()),
     )?;
 
-    let snapshot_folder = Path::new("../../snapshots");
-    // get absolute path
-    let snapshot_folder = snapshot_folder.canonicalize()?;
 
     let extra_args = [
         "--rendering-driver",
@@ -203,7 +206,7 @@ pub fn coverage_with_itest(devmode: bool) -> Result<(), anyhow::Error> {
         "[[52,-52],[52,-54],[52,-56],[52,-58],[52,-60],[52,-62],[52,-64],[52,-66],[52,-68],[54,-52],[54,-54],[54,-56],[54,-58],[54,-60]]",
         "--realm",
         "https://decentraland.github.io/scene-explorer-tests/scene-explorer-tests",
-        "--snapshot_folder",
+        "--snapshot-folder",
         snapshot_folder.to_str().unwrap()
         
     ]
