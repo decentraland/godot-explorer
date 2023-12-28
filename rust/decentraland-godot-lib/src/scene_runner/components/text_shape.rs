0214b5f5e553ab647b19f5380cd8831bba19a98a
Lean Mendoza <leandro@decentraland.org>
2023-12-28 11:23:04 -0300

    wip

--- rust/decentraland-godot-lib/src/scene_runner/components/text_shape.rs
+++ rust/decentraland-godot-lib/src/scene_runner/components/text_shape.rs
@@ -38,9 +38,33 @@ pub fn update_text_shape(scene: &mut Scene, crdt_state: &mut SceneCrdtState) {
                     None => (Label3D::new_alloc(), true),
                 };
 
+                let opacity = new_value
+                    .text_color
+                    .as_ref()
+                    .map(|color| color.a.clone())
+                    .unwrap_or(1.0);
+
+                let text_color = new_value
+                    .text_color
+                    .map(|color| Color::from_rgba(color.r, color.g, color.b, opacity))
+                    .unwrap_or(Color::from_rgba(1.0, 1.0, 1.0, opacity));
+
+                let outline_color = new_value
+                    .outline_color
+                    .map(|color| Color::from_rgba(color.r, color.g, color.b, opacity))
+                    .unwrap_or(Color::from_rgba(1.0, 1.0, 1.0, opacity));
+
+                let shadow_color = new_value
+                    .shadow_color
+                    .map(|color| Color::from_rgba(color.r, color.g, color.b, opacity))
+                    .unwrap_or(Color::from_rgba(1.0, 1.0, 1.0, opacity));
+
                 label_3d.set_text(GString::from(new_value.text));
                 label_3d.set_font_size(12 * new_value.font_size.unwrap_or(3.0) as i32); // TODO: see font size fix
                 label_3d.set_outline_size(8); // TODO: see font size fix
+
+                label_3d.set_modulate(text_color);
+                label_3d.set_outline_modulate(outline_color);
 
                 if add_to_base {
                     label_3d.set_name(GString::from("TextShape"));
