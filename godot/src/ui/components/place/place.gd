79790c03f992ffc163a235d3bdf6fe497db03aa4
Seba Di Lauro <jsdilauro@gmail.com>
2024-02-02 14:51:12 -0300

    lint

--- godot/src/ui/components/place/place.gd
+++ godot/src/ui/components/place/place.gd
@@ -9,11 +9,17 @@ const HOME_OUTLINE = preload("res://assets/ui/Icn/HomeOutline.svg")
 const STAR_OUTLINE = preload("res://assets/ui/Icn/StarOutline.svg")
 const STAR_SOLID = preload("res://assets/ui/Icn/StarSolid.svg")
 
-@onready var button_like = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Like
-@onready var button_dislike = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Dislike
-@onready var button_fav = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Fav
-@onready var button_share = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Share
-@onready var button_home = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Home
+@onready
+var button_like = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Like
+@onready
+var button_dislike = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Dislike
+@onready
+var button_fav = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Fav
+@onready
+var button_share = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Share
+@onready
+var button_home = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/Button_Home
+
 
 func _on_button_like_toggled(toggled_on):
 	if toggled_on:
@@ -21,17 +27,20 @@ func _on_button_like_toggled(toggled_on):
 	else:
 		button_like.icon = LIKE
 
+
 func _on_button_dislike_toggled(toggled_on):
 	if toggled_on:
 		button_dislike.icon = DISLIKE_SOLID
 	else:
 		button_dislike.icon = DISLIKE
 
+
 func _on_button_fav_toggled(toggled_on):
 	if toggled_on:
 		button_fav.icon = STAR_SOLID
 	else:
 		button_fav.icon = STAR_OUTLINE
+
 
 func _on_button_home_toggled(toggled_on):
 	if toggled_on:
