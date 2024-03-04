3e6b78eb55e567aca33bc594f8e700e323552732
Lean Mendoza <leandro@decentraland.org>
2024-03-04 13:30:42 -0300

    wip

--- rust/decentraland-godot-lib/ios-build.sh
+++ rust/decentraland-godot-lib/ios-build.sh
@@ -3,7 +3,7 @@
 export FFMPEG_DIR=~/github/ffmpeg-kit/prebuilt/apple-ios-arm64/ffmpeg
 export RUSTY_V8_MIRROR=https://github.com/leanmendoza/rusty_v8/releases/download
 
-GN_ARGS=use_custom_libcxx=false RUST_BACKTRACE=full cargo build --target aarch64-apple-ios -vv --verbose --no-default-features --features use_deno
+GN_ARGS=use_custom_libcxx=false RUST_BACKTRACE=full cargo build --target aarch64-apple-ios -vv --verbose --no-default-features -F use_deno
 
 mkdir -p ../../godot/lib/ios/
 cp target/aarch64-apple-ios/release/libdecentraland_godot_lib.dylib ../../godot/lib/ios/libdecentraland_godot_lib.dylib
