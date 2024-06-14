e2b119819582a4e4023f1e068c6e0afa825d8955
Mateo "Kuruk" Miccino <mateomiccino@gmail.com>
2024-06-14 19:22:13 -0300

    fix ios

--- rust/decentraland-godot-lib/ios-build.sh
+++ rust/decentraland-godot-lib/ios-build.sh
@@ -3,8 +3,7 @@
 export FFMPEG_DIR=~/github/ffmpeg-kit/prebuilt/apple-ios-arm64/ffmpeg
 export RUSTY_V8_MIRROR=https://github.com/leanmendoza/rusty_v8/releases/download
 
-RUSTFLAGS="-C link-arg=-mios-version-min=12.0" GN_ARGS=use_custom_libcxx=false RUST_BACKTRACE=full cargo build --target aarch64-apple-ios --release --no-default-features -F use_deno
-
+GN_ARGS=use_custom_libcxx=false RUST_BACKTRACE=full cargo build --target aarch64-apple-ios --release --no-default-features -F use_deno -F use_livekit
 mkdir -p ../../godot/lib/ios/
 cp target/aarch64-apple-ios/release/libdecentraland_godot_lib.dylib ../../godot/lib/ios/libdecentralandgodot.dylib
 # Temp
