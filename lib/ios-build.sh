abeb4210fefb5c3c260a2d7b9209e287af037152
Mateo "Kuruk" Miccino <mateomiccino@gmail.com>
2024-06-22 21:08:15 -0300

    fixes

--- lib/ios-build.sh
+++ lib/ios-build.sh
@@ -4,7 +4,5 @@ export FFMPEG_DIR=~/github/ffmpeg-kit/prebuilt/apple-ios-arm64/ffmpeg
 export RUSTY_V8_MIRROR=https://github.com/leanmendoza/rusty_v8/releases/download
 
 GN_ARGS=use_custom_libcxx=false RUST_BACKTRACE=full cargo build --target aarch64-apple-ios --release --no-default-features -F use_deno -F use_livekit
-mkdir -p ../../godot/lib/ios/
-cp target/aarch64-apple-ios/release/libdclgodot.dylib ../../godot/lib/ios/libdclgodot.dylib
-# Temp
-cp target/aarch64-apple-ios/release/libdclgodot.dylib "/Users/kuruk/Projects/ios-export/Decentraland Mobile Client/dylibs/lib/ios/libdclgodot.framework/libdclgodot"
+mkdir -p ../godot/lib/ios/
+cp target/aarch64-apple-ios/release/libdclgodot.dylib ../godot/lib/ios/libdclgodot.dylib
