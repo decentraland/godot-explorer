b506fd3f84716bbca7a17a309e17934a7a858bd6
Lean Mendoza <leandro@decentraland.org>
2024-09-26 14:41:37 -0300

    improve avatar rendering

--- godot/src/tool/avatar_renderer/avatar_renderer_standalone.gd
+++ godot/src/tool/avatar_renderer/avatar_renderer_standalone.gd
@@ -10,7 +10,6 @@ var current_avatar: DclAvatarWireFormat
 
 @onready var avatar_preview = %AvatarPreview
 
-
 # TODO: this can be a command line parser and get some helpers like get_string("--realm"), etc
 func get_params_from_cmd():
 	if USE_TEST_INPUT:
@@ -55,6 +54,13 @@ func _ready():
 
 func start():
 	async_update_avatar(0)
+
+	# Visual enhance
+	var viewport: Viewport = avatar_preview.subviewport.get_viewport()
+	viewport.use_debanding = true
+	viewport.scaling_3d_scale = 2.0
+	RenderingServer.viewport_set_msaa_3d(viewport.get_viewport_rid(), RenderingServer.VIEWPORT_MSAA_8X)
+	RenderingServer.screen_space_roughness_limiter_set_active(true, 4.0, 1.0)
 
 
 func flush_logs():
