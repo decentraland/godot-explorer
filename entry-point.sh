6ec61b3ab7dd2876244b289fefb654fe34cefac6
Mateo Miccino <mateomiccino@gmail.com>
2024-10-21 21:52:11 -0300

    fix: docker build only in main, fix ci running multiple times on PRs and remove avatar tests from coverage (#465)
    * docker build only in main, only executes pushes on main
    
    * remove avatar profile generation tests from coverage
    
    * ignore crashes on docker entrypoint

--- entry-point.sh
+++ entry-point.sh
@@ -2,4 +2,4 @@
 
 /usr/bin/Xvfb -ac :99 -screen 0 1280x1024x24 &
 export DISPLAY=:99
-./decentraland.godot.client.x86_64 --rendering-driver opengl3 --avatar-renderer --avatars avatars.json
+./decentraland.godot.client.x86_64 --rendering-driver opengl3 --avatar-renderer --avatars avatars.json || true
