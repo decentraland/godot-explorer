7ed3f96f4454a0b99862f37722037f410f01f47d
Lean Mendoza <leandro@decentraland.org>
2024-11-20 19:55:31 -0300

    wip

--- godot/src/ui/components/warning_messages/warning_messages.gd
+++ godot/src/ui/components/warning_messages/warning_messages.gd
@@ -13,7 +13,7 @@ func async_create_popup_warning(
 	add_child(popup_warning)
 	var description_length = popup_warning.set_warning(warning_type, title, description)
 
-	var timeout = max(0, float(description_length) / 20.0) # 30 characters per second to read
+	var timeout = max(0, float(description_length) / 20.0)  # 30 characters per second to read
 
 	await get_tree().create_timer(timeout).timeout
 
