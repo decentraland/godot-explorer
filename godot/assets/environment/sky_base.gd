fba55a230d8d6aa788976cd5bd6b82b1a63b7eb4
manuelmaceira <manuelmaceira.m@gmail.com>
2024-11-28 18:22:03 -0300

    Using sky_base to test light levels

--- godot/assets/environment/sky_base.gd
+++ godot/assets/environment/sky_base.gd
@@ -6,22 +6,24 @@ extends Node
 
 
 func _ready():
-	if Global.is_xr():
-		Global.loading_started.connect(self._on_loading_started)
-		Global.loading_finished.connect(self._on_loading_finished)
+	# if Global.is_xr():
+	Global.loading_started.connect(self._on_loading_started)
+	Global.loading_finished.connect(self._on_loading_finished)
 
 
 func _on_loading_started():
+	print("loading started")
 	world_environment.environment.background_energy_multiplier = 0.0
 	world_environment.environment.ambient_light_energy = 0.0
 	sun_light.light_energy = 0.0
 
 
 func _on_loading_finished():
+	print("loading finished")
 	var tween = get_tree().create_tween().set_parallel(true)
 	world_environment.environment.background_energy_multiplier = 0.0
 	world_environment.environment.ambient_light_energy = 0.0
 	sun_light.light_energy = 0.0
 	tween.tween_property(world_environment, "environment:background_energy_multiplier", 1.0, 1.0)
-	tween.tween_property(world_environment, "environment:ambient_light_energy", 1.0, 1.0)
-	tween.tween_property(sun_light, "light_energy", 1.0, 1.0)
+	tween.tween_property(world_environment, "environment:ambient_light_energy", .4, 1.0)
+	tween.tween_property(sun_light, "light_energy", .2, 1.0)
