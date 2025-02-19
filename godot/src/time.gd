4b4774d520db7f6c06a7930ca7c85a8be6528a78
Manuel Maceira <40546535+manuelmaceira@users.noreply.github.com>
2025-02-19 15:41:18 -0300

    feat: better toon, env and new skybox (#515)
    * Better looking environment (bloom) and most materials importing ok
    
    * Using sky_base to test light levels
    
    * cleanup
    
    * Fix albedo on emission textures and better light/env
    
    * Better environments
    
    * Restore procedural sky for low graphics
    
    * Formatter
    
    * runner modificado
    
    * fix runner
    
    * Build android
    
    * Sky shader WIP
    
    * shader modules
    
    * sky
    
    * sky shader tweaks
    
    * cleanup and sky time sync
    
    * very small tweak to avatar materials
    
    * Revert Rust changes
    
    * update snapshots by env (with avatar texture already issue)
    
    * revert some unrelated changes
    
    * collect al the avatars similarity
    
    * fix avatar image generation
    
    * static check fix
    
    * cleanup
    
    * update snapshot
    
    * update snapshots, remove unused files, use environment from avatar preview
    
    * ci update
    
    ---------
    
    Co-authored-by: Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
    Co-authored-by: Mateo Miccino <mateomiccino@gmail.com>

---
+++ godot/src/time.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Time syncing singleton for the sky shader
+
+const TIME_OFFSET = .43
+
+
+func _process(_delta: float) -> void:
+	var time = (
+		int(floor(Time.get_unix_time_from_system()) + Time.get_time_zone_from_system().bias * 60)
+		% 86400
+	)
+	var normalized_time = float(time) / 86400
+	normalized_time += TIME_OFFSET
+	normalized_time -= floor(normalized_time)
+	RenderingServer.global_shader_parameter_set("day_night_cycle", normalized_time)
