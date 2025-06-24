c3b105ab3a584d213b93e2d212f2b5486ea426b2
Manuel Maceira <manuelmaceira.m@gmail.com>
2025-06-24 09:52:29 -0300

    gdformat

--- godot/src/decentraland_components/avatar/nickname_quad.gd
+++ godot/src/decentraland_components/avatar/nickname_quad.gd
@@ -1,17 +1,21 @@
 @tool
 extends Sprite3D
 
-@export_range(0,20) var fade_end = 7.0
-@export_range(0,20) var fade_start = 6.0
+@export_range(0, 20) var fade_end = 7.0
+@export_range(0, 20) var fade_start = 6.0
 
 @onready var camera_3d = get_viewport().get_camera_3d()
+
 
 func _ready():
 	visibility_range_end = fade_end
 
-func _process(_delta:float)->void:
+
+func _process(_delta: float) -> void:
 	# Poll for camera_3d existance
-	if !camera_3d: camera_3d = get_viewport().get_camera_3d() 
-	if !camera_3d: return 
-	var dist : float = camera_3d.global_transform.origin.distance_squared_to(global_transform.origin)
-	modulate = Color(1.0,1.0,1.0,smoothstep(fade_end*fade_end, fade_start*fade_start, dist))
+	if !camera_3d:
+		camera_3d = get_viewport().get_camera_3d()
+	if !camera_3d:
+		return
+	var dist: float = camera_3d.global_transform.origin.distance_squared_to(global_transform.origin)
+	modulate = Color(1.0, 1.0, 1.0, smoothstep(fade_end * fade_end, fade_start * fade_start, dist))
