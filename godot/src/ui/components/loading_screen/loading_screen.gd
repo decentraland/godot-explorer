01100ab49e4c4f76a9021f8ac1dc8c21f5df5da3
Manuel Maceira <manuelmaceira.m@gmail.com>
2025-07-14 16:53:34 -0300

    xtask error workaround

--- godot/src/ui/components/loading_screen/loading_screen.gd
+++ godot/src/ui/components/loading_screen/loading_screen.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const CHRONOMETER = preload("res://src/utils/chronometer.gd")
+
 var bg_colors: Array[Color] = [
 	Color(0.5, 0.25, 0.0, 1.0),
 	Color(0.0, 0.0, 0.5, 1.0),
@@ -33,7 +35,7 @@ var carousel = $VBox_Loading/ColorRect_Background/Control_Discover/VBoxContainer
 
 @onready var loading_screen_progress_logic = $LoadingScreenProgressLogic
 @onready var timer_check_progress_timeout = $Timer_CheckProgressTimeout
-@onready var debug_chronometer := preload("res://src/utils/chronometer.gd").new()
+@onready var debug_chronometer := CHRONOMETER.new()
 
 
 func _ready():
@@ -47,7 +49,7 @@ func _ready():
 # Forward
 func enable_loading_screen():
 	if !debug_chronometer:
-		debug_chronometer = Chronometer.new()
+		debug_chronometer = CHRONOMETER.new()
 	debug_chronometer.restart("Starting to load scene")
 	Global.loading_started.emit()
 	Global.release_mouse()
