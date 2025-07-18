68656e71ac1e83ecd04e3e9b2fc55ec15e333e87
Manuel Maceira <40546535+manuelmaceira@users.noreply.github.com>
2025-07-18 16:39:22 -0300

    feat: Dev tools menu and performance benchmark (#598)
    * WARN level filter
    
    * dev tools
    
    * Measure timings and renderdoc integration
    
    * resource counter tool and manual benchmark working
    
    * --headless --dcl-benchmark works
    
    * add gitignore to renderdoc artifacts
    
    * move renderdoc config to editosettings
    
    * Make sure it closes after
    
    * Remove singleton logic, use flags directly
    
    * xtask error workaround
    
    * xtask error workaround
    
    * update snapshots and revert workaround
    
    * update snapshots and revert project.godot
    
    * remove unused var
    
    * fix snapshots
    
    ---------
    
    Co-authored-by: Mateo "Kuruk" Miccino <mateomiccino@gmail.com>

---
+++ godot/addons/dcl_dev_tools/dev_tools/resource_counter/tool.gd
@@ -0,0 +1,55 @@
+extends "res://addons/dcl_dev_tools/dev_tools/dcl_dev_tool.gd"
+
+const SINGLETON_NAME = "ResourceCounter"
+
+var places = {
+	"Genesis Plaza": Vector2i(0, 0),
+	"Soul Magic": Vector2i(96, -110),
+	"Tower of Hanoi": Vector2i(61, -22),
+	"Meta gamimall": Vector2i(1, 95),
+	"Wondermine": Vector2i(-29, 55),
+	"Exodus": Vector2i(125, 41),
+	"BitCinema": Vector2i(-109, -93),
+	"DAO HQ": Vector2i(65, 13),
+	"Casa Roustan": Vector2i(37, -114),
+	"Fashion Week Scene": Vector2i(142, -78),
+	"Game Night": Vector2i(1, 81)
+}
+
+
+func populate_menu(menu: PopupMenu, id: int):
+	menu.add_item("Run Benchmark", id)
+
+	var args := OS.get_cmdline_args()
+	if args.has("--dcl-benchmark"):
+		print("Running Benchmark...")
+		await execute()
+		plugin.get_editor_interface().get_editor_main_screen().get_tree().quit()
+
+
+func execute():
+	var interface = plugin.get_editor_interface()
+
+	var old_args = ProjectSettings.get("editor/run/main_run_args")
+	for place in places.keys():
+		print("Launching: %s" % place)
+		var coord = places[place]
+		(
+			ProjectSettings
+			. set(
+				"editor/run/main_run_args",
+				(
+					"--measure-perf --skip-lobby --realm https://realm-provider.decentraland.org/main --location %d,%d"
+					% [coord.x, coord.y]
+				)
+			)
+		)
+
+		# If i don't wait settings changes may not impact
+		await plugin.get_tree().create_timer(.1).timeout
+
+		plugin.get_editor_interface().play_main_scene()
+		while interface.get_playing_scene() != "":
+			await plugin.get_tree().create_timer(.1).timeout
+
+	ProjectSettings.set("editor/run/main_run_args", old_args)
---
+++ godot/addons/dcl_dev_tools/tools.gd
@@ -0,0 +1,52 @@
+@tool
+extends EditorPlugin
+
+var tools = [
+	preload("./dev_tools/test_runner/tool.gd").new(self),
+	preload("./dev_tools/typical_places/tool.gd").new(self),
+	preload("./dev_tools/renderdoc/tool.gd").new(self),
+	preload("./dev_tools/resource_counter/tool.gd").new(self)
+]
+
+var custom_menu: MenuButton
+var menu_name = "DCL Tools"
+
+
+func _enter_tree() -> void:
+	custom_menu = MenuButton.new()
+	custom_menu.name = "DCL_ToolsMenu"
+	custom_menu.text = menu_name
+
+	initialize_items()
+	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, custom_menu)
+	call_deferred("_position_menu")
+
+
+func initialize_items():
+	var popup = custom_menu.get_popup()
+	for tool_id in tools.size():
+		tools[tool_id].populate_menu(popup, tool_id)
+	popup.id_pressed.connect(_on_menu_item_selected)
+
+
+func _on_menu_item_selected(id: int) -> void:
+	assert(tools[id], "No tool found for that item!")
+	tools[id].execute()
+
+
+func _exit_tree() -> void:
+	if !custom_menu:
+		return
+	var container = custom_menu.get_parent().get_parent()
+	custom_menu.get_parent().remove_child(custom_menu)
+	container.add_child(custom_menu)
+	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, custom_menu)
+	custom_menu.queue_free()
+	for tool in tools:
+		tool.cleanup()
+
+
+func _position_menu() -> void:
+	var menu_bar := custom_menu.get_parent()
+	menu_bar.remove_child(custom_menu)
+	menu_bar.get_child(1).add_child(custom_menu)
