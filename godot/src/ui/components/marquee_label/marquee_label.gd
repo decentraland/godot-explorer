90c665ce17b7d6e3ddb74d48c2ccca17ed215388
SebaDL <jsdilauro@gmail.com>
2025-07-23 11:41:46 -0300

    add marquee_label component, add marquee_label to selected equipped_item on profile

---
+++ godot/src/ui/components/marquee_label/marquee_label.gd
@@ -0,0 +1,66 @@
+extends ScrollContainer
+
+var is_marquee_active: bool = false
+var marquee_tween: Tween
+var marquee_speed: float = 60.0
+var pause_duration: float = 2
+@export var label_settings: LabelSettings
+@onready var label: Label = %Label
+
+func _ready() -> void:
+	label.label_settings = label_settings
+
+
+func set_text(text:String)-> void:
+	label.text = text
+
+func is_text_overflowing() -> bool:
+	return label.size.x > size.x
+
+
+func start_marquee_effect() -> void:
+	if is_marquee_active:
+		return
+
+	is_marquee_active = true
+
+	var max_scroll_distance = label.size.x - size.x
+	if max_scroll_distance <= 0:
+		return
+
+	var scroll_duration = max_scroll_distance / marquee_speed
+
+	if marquee_tween:
+		marquee_tween.kill()
+
+	label.position.x = 0
+
+	marquee_tween = create_tween()
+	marquee_tween.set_loops()
+	marquee_tween.set_trans(Tween.TRANS_LINEAR)
+	marquee_tween.set_ease(Tween.EASE_IN_OUT)
+
+	marquee_tween.tween_interval(pause_duration)
+	marquee_tween.tween_property(label, "position:x", -max_scroll_distance, scroll_duration)
+	marquee_tween.tween_interval(pause_duration)
+	marquee_tween.tween_property(label, "position:x", 0, scroll_duration)
+
+
+func check_and_start_marquee() -> void:
+	if is_text_overflowing():
+		start_marquee_effect()
+	else:
+		label.position.x = 0
+
+
+func stop_marquee_effect() -> void:
+	if not is_marquee_active:
+		return
+
+	is_marquee_active = false
+
+	if marquee_tween:
+		marquee_tween.kill()
+		marquee_tween = null
+
+	label.position.x = 0
--- godot/src/ui/components/profile/profile_equipped_item.gd
+++ godot/src/ui/components/profile/profile_equipped_item.gd
@@ -1,11 +1,11 @@
 extends Control
 
 @onready var item_preview: ItemPreview = %ItemPreview
-@onready var label_name: Label = %Label_Name
 @onready var label_rarity: Label = %Label_Rarity
 @onready var button_buy: Button = %Button_Buy
 @onready var margin_container: MarginContainer = $MarginContainer
 @onready var panel_container_rarity: PanelContainer = %PanelContainer_Rarity
+@onready var marquee_label_name: ScrollContainer = %MarqueeLabel_Name
 
 var is_pressed:bool = false
 var is_buyable:bool = true
@@ -17,7 +17,7 @@ func _ready():
 
 func async_set_item(item:DclItemEntityDefinition):
 	item_preview.async_set_item(item)
-	label_name.text = item.get_display_name()
+	marquee_label_name.set_text(item.get_display_name())
 	var rarity = item.get_rarity()
 	if rarity.length() != 0:
 		label_rarity.text = rarity.to_upper()
@@ -29,26 +29,28 @@ func async_set_item(item:DclItemEntityDefinition):
 func set_base_emote(urn:String):
 	item_preview.set_base_emote_info(urn)
 	label_rarity.text = 'BASE'
-	label_name.text = Emotes.DEFAULT_EMOTE_NAMES[urn]
+	marquee_label_name.set_text(Emotes.DEFAULT_EMOTE_NAMES[urn])
 	is_buyable = false
 
 
 func _update_view() -> void:
-	var margin_pressed = 0
-	var margin_unpressed = 15
+	#var margin_pressed = 0
+	#var margin_unpressed = 15
 	if is_pressed:
+		marquee_label_name.check_and_start_marquee()
 		if is_buyable:
 			button_buy.show()
-		margin_container.add_theme_constant_override("margin_top", margin_pressed)
-		margin_container.add_theme_constant_override("margin_left", margin_pressed)
-		margin_container.add_theme_constant_override("margin_bottom", margin_pressed)
-		margin_container.add_theme_constant_override("margin_right", margin_pressed)
+		#margin_container.add_theme_constant_override("margin_top", margin_pressed)
+		#margin_container.add_theme_constant_override("margin_left", margin_pressed)
+		#margin_container.add_theme_constant_override("margin_bottom", margin_pressed)
+		#margin_container.add_theme_constant_override("margin_right", margin_pressed)
 	else:
 		button_buy.hide()
-		margin_container.add_theme_constant_override("margin_top", margin_unpressed)
-		margin_container.add_theme_constant_override("margin_left", margin_unpressed)
-		margin_container.add_theme_constant_override("margin_bottom", margin_unpressed)
-		margin_container.add_theme_constant_override("margin_right", margin_unpressed)
+		marquee_label_name.stop_marquee_effect()
+		#margin_container.add_theme_constant_override("margin_top", margin_unpressed)
+		#margin_container.add_theme_constant_override("margin_left", margin_unpressed)
+		#margin_container.add_theme_constant_override("margin_bottom", margin_unpressed)
+		#margin_container.add_theme_constant_override("margin_right", margin_unpressed)
 
 
 func _on_toggled(toggled_on: bool) -> void:
