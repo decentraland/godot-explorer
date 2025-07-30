3dd7a000aeac1f30e3ba45b4cb4ab427dbe431db
Manuel Maceira <manuelmaceira.m@gmail.com>
2025-07-30 11:50:46 -0300

    Sync fog color with time of day and remove warning

--- godot/assets/environment/sky_base.gd
+++ godot/assets/environment/sky_base.gd
@@ -4,6 +4,9 @@ extends Node
 var last_time := 0.0
 
 @onready var world_environment: WorldEnvironment = $WorldEnvironment
+@onready var sky_material = world_environment.environment.sky.sky_material
+@onready var day_fog_color = sky_material.get_shader_parameter("clouds_gradient_day").gradient.colors[0]
+@onready var night_fog_color = sky_material.get_shader_parameter("clouds_gradient_night").gradient.colors[0]
 
 
 func _ready():
@@ -20,7 +23,6 @@ func on_scene_runner_child_entered_tree(node: Node3D):
 
 
 func _on_loading_started():
-	print("loading started")
 	world_environment.environment.background_energy_multiplier = 0.0
 	world_environment.environment.ambient_light_energy = 0.0
 
@@ -35,10 +37,15 @@ func _on_loading_finished():
 	scene_runner.child_entered_tree.disconnect(self.on_scene_runner_child_entered_tree)
 	for child in scene_runner.get_children():
 		child.show()
-	print("loading finished")
 	var tween = get_tree().create_tween().set_parallel(true)
 	world_environment.environment.background_energy_multiplier = 0.0
 	world_environment.environment.ambient_light_energy = 0.0
 
 	tween.tween_property(world_environment, "environment:background_energy_multiplier", 1.0, 1.0)
 	tween.tween_property(world_environment, "environment:ambient_light_energy", 1.0, 1.0)
+
+func _process(delta: float) -> void:
+	var cycle = Global.skybox_time.get_normalized_time() + .43
+	cycle -= floor(cycle)
+	var blend = sin(cycle * PI);
+	world_environment.environment.fog_light_color = day_fog_color.lerp(night_fog_color, blend)
