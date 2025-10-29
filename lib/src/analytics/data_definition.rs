a5b575f673b181840491b9b1dbadf0ddee80dab4
Sebastián Di Lauro <seba@dclregenesislabs.xyz>
2025-10-29 16:57:41 -0300

    feat: events metrics (#790)
    * wip
    
    * fix format and lint
    
    * fix jump to event button in portrait view
    
    * add extra_properties to SegmentEventScreenViewed, use old segments instead create new ones
    
    * fix api link
    
    * cargo format
    
    * remove unnecessary definitions

--- lib/src/analytics/data_definition.rs
+++ lib/src/analytics/data_definition.rs
@@ -214,6 +214,9 @@ pub struct SegmentEventClickButton {
 pub struct SegmentEventScreenViewed {
     // Name of the screen viewed.
     pub screen_name: String,
+    // JSON with extra payload, in case we need to track additional metadata.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub extra_properties: Option<String>,
 }
 
 pub fn build_segment_event_batch_item(
