734b7776e89b4af326268102c741ff00f00832e2
Mateo "Kuruk" Miccino <mateomiccino@gmail.com>
2025-11-11 20:47:22 -0300

    fixes calendar, disable mic

--- godot/src/ui/components/notifications/notification_content.gd
+++ godot/src/ui/components/notifications/notification_content.gd
@@ -37,8 +37,7 @@ func _update_ui() -> void:
 	# Wrap title in [b] tags for bold
 	var title_text = NotificationTextHelper.get_notification_header(notif_type, metadata)
 	label_title.text = "[b]" + title_text + "[/b]"
-	var description: String = NotificationTextHelper.get_notification_title(notif_type, metadata)
-	label_description.text = description
+	label_description.text = NotificationTextHelper.get_notification_title(notif_type, metadata)
 
 	# Apply profile-style background for friend notifications first (before loading images)
 	_apply_friend_notification_styling(notif_type, metadata)
