d34f889382882e01b096c80704cedeffd6a66860
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2025-12-08 15:33:12 -0300

    feat: add send binary support (#919)
    * feat: add send binary support
    
    * fix

--- lib/src/scene_runner/rpc_calls/mod.rs
+++ lib/src/scene_runner/rpc_calls/mod.rs
@@ -91,13 +91,11 @@ pub fn process_rpcs(scene: &mut Scene, current_parcel_scene_id: &SceneId, rpc_ca
                     .bind()
                     .send_async(body, response);
             }
-            RpcCall::SendCommsMessage { body } => {
+            RpcCall::SendCommsMessage { body, recipient } => {
                 let scene_id = scene.scene_entity_definition.id.clone();
                 let mut comms = DclGlobal::singleton().bind().get_comms();
                 let mut communication_manager = comms.bind_mut();
-                for data in body {
-                    communication_manager.send_scene_message(scene_id.clone(), data);
-                }
+                communication_manager.send_scene_message(scene_id, body, recipient);
             }
             RpcCall::GetTextureSize { src, response } => {
                 let mut rpc_sender = DclRpcSenderGetTextureSize::new_gd();
