cf9aac70b4bc5484b0021b4b21af47256bb1b368
Mateo "Kuruk" Miccino <mateomiccino@gmail.com>
2025-12-12 14:01:22 -0300

    fix

--- godot/src/ui/components/backpack/backpack.gd
+++ godot/src/ui/components/backpack/backpack.gd
@@ -4,7 +4,7 @@ extends Control
 const WEARABLE_ITEM_INSTANTIABLE = preload(
 	"res://src/ui/components/wearable_item/wearable_item.tscn"
 )
-const FILTER: Texture = preload("res://assets/ui/filter.svg")
+const FILTER: Texture = preload("res://assets/ui/Filter.svg")
 
 @export var hide_navbar: bool = false
 
