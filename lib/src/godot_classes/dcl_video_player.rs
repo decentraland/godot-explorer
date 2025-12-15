89b27883f9949e1b726b577d08d5b9dfde1b1909
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2025-12-15 13:08:15 -0300

    feat: livekit video streaming for `VideoPlayer` component (#920)
    * first iteration
    remove ffmpeg and use_ffmpeg feature
    implement video track subscription
    
    * audio works
    
    * wip, volume working
    remove hardcode src
    
    * cherry pick fix sendBinary
    
    * fix audio jitter
    
    * fix format/clippy
    
    * clean up ffmpeg dep
    
    * format
    
    ---------
    
    Co-authored-by: Mateo "Kuruk" Miccino <mateomiccino@gmail.com>

--- lib/src/godot_classes/dcl_video_player.rs
+++ lib/src/godot_classes/dcl_video_player.rs
@@ -5,8 +5,10 @@ use godot::prelude::*;
 #[class(init, base=AudioStreamPlayer)]
 pub struct DclVideoPlayer {
     // Used to mute and restore the volume
-    #[export]
     dcl_volume: f32,
+
+    // Track muted state to know if we should apply volume changes
+    is_muted: bool,
 
     #[export]
     dcl_source: GString,
@@ -32,11 +34,35 @@ impl DclVideoPlayer {
         let _ = sender.send(file_path.to_string());
     }
 
+    #[func]
+    pub fn get_dcl_volume(&self) -> f32 {
+        self.dcl_volume
+    }
+
+    #[func]
+    pub fn set_dcl_volume(&mut self, value: f32) {
+        self.dcl_volume = value;
+        // Update the actual audio volume if not muted
+        if !self.is_muted {
+            let db_volume = if value <= 0.0 {
+                -80.0
+            } else {
+                20.0 * f32::log10(value)
+            };
+            self.base_mut().set_volume_db(db_volume);
+        }
+    }
+
     pub fn set_muted(&mut self, value: bool) {
+        self.is_muted = value;
         if value {
             self.base_mut().set_volume_db(-80.0);
         } else {
-            let db_volume = 20.0 * f32::log10(self.get_dcl_volume());
+            let db_volume = if self.dcl_volume <= 0.0 {
+                -80.0
+            } else {
+                20.0 * f32::log10(self.dcl_volume)
+            };
             self.base_mut().set_volume_db(db_volume);
         }
     }
