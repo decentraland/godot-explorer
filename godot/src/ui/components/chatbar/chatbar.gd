5808b91ffd3718a26d804dc5c96ba9a541fc37b4
Eibriel <eibriel@eibriel.com>
2025-12-22 16:37:59 -0300

    feat: add haptic feedback to mobile ui interactions (#1005)
    * adding haptic feedback
    
    * removing unused signal connection on menu

--- godot/src/ui/components/chatbar/chatbar.gd
+++ godot/src/ui/components/chatbar/chatbar.gd
@@ -5,11 +5,14 @@ signal share_place
 
 func _on_hud_button_discover_pressed() -> void:
 	Global.open_discover.emit()
+	Global.send_haptic_feedback()
 
 
 func _on_hud_button_share_pressed() -> void:
 	share_place.emit()
+	Global.send_haptic_feedback()
 
 
 func _on_button_chat_pressed() -> void:
 	Global.open_chat.emit()
+	Global.send_haptic_feedback()
