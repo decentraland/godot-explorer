ce8794972502f112ac6fbc7d23b74ce549c2d07b
SebaDL <seba@dclregenesislabs.xyz>
2026-01-07 11:32:22 -0300

    add recursive function to add attenuation for all the joypad buttons

--- godot/src/ui/components/joypad/joypad.gd
+++ godot/src/ui/components/joypad/joypad.gd
@@ -4,27 +4,19 @@ var combo_opened: bool = false
 
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var button_combo: Button = %Button_Combo
-@onready var button_press: Button = %Button_Press
-@onready var button_jump: Button = %Button_Jump
-@onready var button_primary: Button = %Button_Primary
-@onready var button_secondary: Button = %Button_Secondary
-@onready var button_combo_1: Button = %Button_Combo1
-@onready var button_combo_2: Button = %Button_Combo2
-@onready var button_combo_3: Button = %Button_Combo3
-@onready var button_combo_4: Button = %Button_Combo4
 
 
 func _ready() -> void:
 	Global.close_combo.connect(_close_because_action)
-	button_combo.set_meta("attenuated_sound", true)
-	button_press.set_meta("attenuated_sound", true)
-	button_jump.set_meta("attenuated_sound", true)
-	button_primary.set_meta("attenuated_sound", true)
-	button_secondary.set_meta("attenuated_sound", true)
-	button_combo_1.set_meta("attenuated_sound", true)
-	button_combo_2.set_meta("attenuated_sound", true)
-	button_combo_3.set_meta("attenuated_sound", true)
-	button_combo_4.set_meta("attenuated_sound", true)
+	_set_attenuated_sound_for_buttons(self)
+
+
+func _set_attenuated_sound_for_buttons(node: Node) -> void:
+	if node is Button:
+		node.set_meta("attenuated_sound", true)
+
+	for child in node.get_children():
+		_set_attenuated_sound_for_buttons(child)
 
 
 func _on_button_combo_toggled(toggled_on: bool) -> void:
