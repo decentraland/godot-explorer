8ba50d3941cf77a9b7589e92ed4beb5f7f54287a
SebaDL <seba@dclregenesislabs.xyz>
2026-01-08 14:24:47 -0300

    check orientation only on ready

--- godot/src/ui/components/utils/clean_orientation.gd
+++ godot/src/ui/components/utils/clean_orientation.gd
@@ -5,23 +5,18 @@ extends Control
 
 
 func _ready() -> void:
-	get_window().size_changed.connect(self._on_size_changed)
-	_on_size_changed()
+	_check_orientation_and_clean.call_deferred()
 
 
-func _on_size_changed():
+func _check_orientation_and_clean() -> void:
 	var window_size: Vector2i = DisplayServer.window_get_size()
 	var is_landscape: bool = window_size.x > window_size.y
 	var is_portrait: bool = window_size.y > window_size.x
 
 	if clean_on_landscape and is_landscape:
-		if get_window().size_changed.is_connected(self._on_size_changed):
-			get_window().size_changed.disconnect(self._on_size_changed)
 		queue_free()
 		return
 
 	if clean_on_portrait and is_portrait:
-		if get_window().size_changed.is_connected(self._on_size_changed):
-			get_window().size_changed.disconnect(self._on_size_changed)
 		queue_free()
 		return
