c19d1ed0d2dc9ed3da609354284c22ffd1683781
SebaDL <seba@dclregenesislabs.xyz>
2026-01-08 16:40:58 -0300

    add verification of the nickname when it is changed from the profile

--- godot/src/ui/components/change_nick_popup.gd
+++ godot/src/ui/components/change_nick_popup.gd
@@ -7,8 +7,12 @@ var new_nickname: String
 @onready var button_cancel: Button = %Button_Cancel
 @onready var button_save: Button = %Button_Save
 @onready var button_claim_name: Button = %Button_ClaimName
-@onready var dcl_text_edit_new_nick: VBoxContainer = %DclTextEdit_NewNick
 @onready var label_tag: Label = %Label_Tag
+@onready var line_edit_new_name: LineEdit = %LineEdit_NewName
+@onready var label_advise: Label = %Label_Advise
+@onready var label_error: RichTextLabel = %Label_Error
+@onready var label_length: Label = %Label_Length
+@onready var panel_container_error_border: PanelContainer = %PanelContainer_ErrorBorder
 
 
 func _on_gui_input(event: InputEvent) -> void:
@@ -18,7 +22,7 @@ func _on_gui_input(event: InputEvent) -> void:
 
 
 func close() -> void:
-	dcl_text_edit_new_nick.text_edit.text = ""
+	line_edit_new_name.text = ""
 	hide()
 
 
@@ -27,25 +31,37 @@ func open() -> void:
 	var address = profile.get_ethereum_address()
 	new_nickname = profile.get_name()
 	label_tag.text = "#" + address.substr(address.length() - 4, 4)
-	dcl_text_edit_new_nick.set_text(new_nickname)
+	line_edit_new_name.set_text(new_nickname)
 	_check_error()
 	show()
 
 
 func _check_error() -> void:
-	if dcl_text_edit_new_nick.error or dcl_text_edit_new_nick.text_edit.text.length() <= 0:
-		button_save.disabled = true
+	var color: Color = Color.WHITE
+	label_length.text = (
+		str(line_edit_new_name.text.length()) + "/" + str(line_edit_new_name.character_limit)
+	)
+	if line_edit_new_name.text.length() > line_edit_new_name.character_limit:
+		color = Color.RED
 	else:
-		button_save.disabled = false
+		color = Color.WHITE
+	label_length.label_settings.font_color = color
+
+	if line_edit_new_name.error:
+		label_error.show()
+		label_advise.hide()
+		label_error.text = line_edit_new_name.error_message
+		button_save.disabled = true
+		panel_container_error_border.self_modulate = Color.RED
+	else:
+		label_error.hide()
+		label_advise.show()
+		button_save.disabled = line_edit_new_name.text.is_empty()
+		panel_container_error_border.self_modulate = Color.TRANSPARENT
 
 
 func _on_button_new_link_cancel_pressed() -> void:
 	close()
-
-
-func _on_dcl_text_edit_new_nick_dcl_text_edit_changed() -> void:
-	new_nickname = dcl_text_edit_new_nick.text_edit.text
-	_check_error()
 
 
 func _on_button_cancel_pressed() -> void:
@@ -57,3 +73,8 @@ func _on_button_save_pressed() -> void:
 	ProfileHelper.async_save_profile()
 	emit_signal("update_name_on_profile", new_nickname)
 	close()
+
+
+func _on_line_edit_new_name_dcl_line_edit_changed() -> void:
+	new_nickname = line_edit_new_name.text
+	_check_error()
