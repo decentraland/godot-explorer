014db9305146f2cac6f2b2102922394ad60b4240
Eibriel <eibriel@eibriel.com>
2026-01-14 18:18:36 -0300

    small fixes on PlaceholderManager

--- godot/src/tool/placeholder_manager.gd
+++ godot/src/tool/placeholder_manager.gd
@@ -17,17 +17,33 @@ func _init(_placeholder: Node) -> void:
 	placeholder = _placeholder
 
 
+## If state is LOADING waits for the instance to load.
+## If state is LOADED returns the instance.
+## If state is SLEEPING cancel sleep and returns the instance.
+## If status is UNLOADED creates a new instance.
 func _async_instantiate() -> Node:
-	return await instantiate()
+	if status == STATUS.LOADED:
+		return instance
+	elif status == STATUS.SLEEPING:
+		status = STATUS.LOADED
+		return instance
+	elif status == STATUS.LOADING:
+		while 1:
+			await Engine.get_main_loop().process_frame
+			if status == STATUS.LOADED or instance:
+				return instance
+	
+	status = STATUS.LOADING
+	return await _instantiate()
 
 
-func instantiate() -> Node:
-	if status != STATUS.UNLOADED: return instance
+## Creates and instance from the placeholder
+func _instantiate() -> Node:
 	if not instance:
-		status = STATUS.LOADING
 		instance = placeholder.create_instance()
 		status = STATUS.LOADED
 	return instance
+
 
 ## Starts a timer to free the instance
 func put_to_sleep() -> void:
@@ -36,14 +52,17 @@ func put_to_sleep() -> void:
 	var start_time = Time.get_ticks_msec()
 	while 1:
 		await Engine.get_main_loop().process_frame
-		if Time.get_ticks_msec() - start_time > sleep_time:
-			break
 		if status != STATUS.SLEEPING:
 			return
+		if Time.get_ticks_msec() - start_time > sleep_time:
+			break
 	_remove_instance()
 
+
+## Frees the instance
 func _remove_instance() -> void:
 	if status != STATUS.SLEEPING: return
 	if instance:
 		instance.queue_free()
 		instance = null
+	status = STATUS.UNLOADED
