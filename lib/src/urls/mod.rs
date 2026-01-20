50b90eb9f0f4a4641a1c73eb62f7c8067a8d7ef0
Mateo "Kuruk" Miccino <mateomiccino@gmail.com>
2026-01-20 09:09:03 -0300

    chore: remove license headers from url modules

--- lib/src/urls/mod.rs
+++ lib/src/urls/mod.rs
@@ -1,9 +1,3 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- */
-
 //! Centralized URL management for Decentraland services.
 //!
 //! URLs are automatically transformed based on the current environment (org, zone, today).
