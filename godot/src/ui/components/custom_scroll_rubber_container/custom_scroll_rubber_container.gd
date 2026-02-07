9a61f3b173ee8f1a7dcfde795ba79e286f8a891f
Eibriel <eibriel@eibriel.com>
2026-02-07 12:04:22 -0300

    rubber scroll wip

--- godot/src/ui/components/custom_scroll_rubber_container/custom_scroll_rubber_container.gd
+++ godot/src/ui/components/custom_scroll_rubber_container/custom_scroll_rubber_container.gd
@@ -16,6 +16,8 @@ var child_drag_position: Vector2
 var child_physics_position: Vector2
 var drag_tween: Tween
 var is_touching := false
+var is_scrolling_mouse := false
+var is_scrolling_touch := false
 
 var velocity: Vector2
 var force: Vector2
@@ -64,22 +66,65 @@ func _notification(what):
 		custom_minimum_size.y = child_min_size.y
 		fit_child_in_rect(c, Rect2(child_position, child_min_size))
 
-
+"""
+## Destroy all events inside ScrollRubber
+## that are unrelated to scrolling
 func _input(event: InputEvent) -> void:
+	if not event.get("position"): return
+	if not get_global_rect().has_point(event.position):
+		return
+	if event is InputEventMouseButton:
+		if event.is_pressed:
+			is_scrolling_mouse = false
+		if is_scrolling_mouse:
+			accept_event()
+			print("Destroy event mouse!")
+	if event is InputEventScreenTouch:
+		if is_scrolling_touch:
+			accept_event()
+			print("Destroy event touch!")
+		if not event.pressed:
+			is_touching = false
+			is_scrolling_mouse = false
+"""
+
+## Cancelling on _gui_input is not working
+
+func _gui_input(event: InputEvent) -> void:
+	accept_event()
+	get_viewport().set_input_as_handled()
 	if not is_valid_child(): return
 	var c: Control = get_child(0)
+	c.accept_event()
+	if event is InputEventMouseButton:
+		if event.pressed:
+			is_scrolling_mouse = false
+			accept_event()
+			print("Mouse pressed")
+		else:
+			if is_scrolling_mouse:
+				accept_event()
+			is_scrolling_mouse = false
 	if event is InputEventScreenTouch:
-		
-		if event.pressed and get_global_rect().has_point(event.position):
+		if event.pressed:
+			accept_event()
 			is_touching = true
+			is_scrolling_touch = false
 			start_pos = event.position
 			child_drag_position = c.position
 			previous_position = c.position
+			print("Touch pressed")
 		else:
+			if is_scrolling_touch:
+				accept_event()
 			is_touching = false
+			is_scrolling_touch = false
 	elif event is InputEventScreenDrag:
 		if is_touching:
 			offset = event.position - start_pos
+			if offset.length() > 50:
+				is_scrolling_mouse = true
+				is_scrolling_touch = true
 			queue_sort()
 
 
