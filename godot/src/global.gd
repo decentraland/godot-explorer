139aacfd9bbb6a9b380e35fe8ae9ed28440a8b78
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2026-02-21 09:39:53 -0300

    test

--- godot/src/global.gd
+++ godot/src/global.gd
@@ -53,8 +53,8 @@ const FORCE_TEST_LOCATION = Vector2i(54, -55)
 #const FORCE_TEST_ARG = "[[52,-56]]"
 # const FORCE_TEST_REALM = "http://localhost:8000"
 
-const FORCE_DEEPLINK = ""
-#const FORCE_DEEPLINK = "decentraland://open?dclenv=today"
+#const FORCE_DEEPLINK = ""
+const FORCE_DEEPLINK = "decentraland://open?dclenv=zone"
 
 # Increase this value for new terms and conditions
 const TERMS_AND_CONDITIONS_VERSION: int = 1
