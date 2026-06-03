91886640d3a3001143a2f52f99db23895092b235
Sebatián Di Lauro <jsdilauro@gmail.com>
2026-06-03 09:58:22 -0300

    fix: cargo fmt

--- lib/src/urls/mod.rs
+++ lib/src/urls/mod.rs
@@ -220,7 +220,10 @@ pub fn marketplace() -> String {
     format!("https://decentraland.{}/marketplace", default_suffix())
 }
 pub fn marketplace_catalog_api() -> String {
-    format!("https://marketplace-api.decentraland.{}/v2/catalog", default_suffix())
+    format!(
+        "https://marketplace-api.decentraland.{}/v2/catalog",
+        default_suffix()
+    )
 }
 pub fn marketplace_claim_name() -> String {
     format!(
