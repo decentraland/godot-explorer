9bae23f447c36924abcfc3ab13d20319d87e41a8
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2026-06-09 17:17:04 -0300

    test

--- godot/src/global.gd
+++ godot/src/global.gd
@@ -65,7 +65,7 @@ const FORCE_TEST_LOCATION = Vector2i(54, -55)
 #const FORCE_TEST_ARG = "[[52,-56]]"
 # const FORCE_TEST_REALM = "http://localhost:8000"
 
-const FORCE_DEEPLINK = "decentraland://open?iap_enabled=true&dclenv=creditsserver::zone,org"
+const FORCE_DEEPLINK = "decentraland://open?iap_enabled=true&dclenv=zone"
 #const FORCE_DEEPLINK = ""
 #const FORCE_DEEPLINK = "decentraland://open?rust-log=dclgodot::analytics::metrics=debug,warn"
 #const FORCE_DEEPLINK = "decentraland://open?dclenv=zone&fake-owned-wearables=urn:decentraland:amoy:collections-v2:0x81004ea82f4af8337e357bef49cc746fce881dee:5"
