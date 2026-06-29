936fb0590fcd0b21a5a7deda2f8249ccfee5b256
Sebatián Di Lauro <jsdilauro@gmail.com>
2026-06-29 18:52:20 -0300

    cleaning code according to the new design

--- godot/src/ui/pages/loading_screen/loading_screen.gd
+++ godot/src/ui/pages/loading_screen/loading_screen.gd
@@ -4,18 +4,6 @@ extends Control
 ## the "RUN ANYWAY" modal is shown regardless of download activity.
 const MAX_LOADING_TIME_SECONDS := 90.0
 
-var bg_colors: Array[Color] = [
-	Color(0.5, 0.25, 0.0, 1.0),
-	Color(0.0, 0.0, 0.5, 1.0),
-	Color(0.5, 0.5, 0.0, 1.0),
-	Color(0.5, 0.0, 0.25, 1.0),
-	Color(0.25, 0.0, 0.5, 1.0),
-	Color(0.34, 0.22, 0.15, 1.0),
-	Color(0.0, 0.5, 0.5, 1.0),
-]
-
-var item_index = 0
-var item_count = 0
 var progress: float = 0.0
 var last_activity_time := Time.get_ticks_msec()
 var loading_start_time := 0
@@ -31,22 +19,19 @@ var loaded_resources_offset := 0
 
 @onready
 var carousel = $VBox_Loading/ColorRect_Background/Control_Discover/VBoxContainer/HBoxContainer_Content/CarouselViewport/SubViewport/Carousel
-@onready var background: ColorRect = $VBox_Loading/ColorRect_Background
 
 @onready var timer_auto_move_carousel = $Timer_AutoMoveCarousel
 
 @onready var loading_screen_progress_logic = $LoadingScreenProgressLogic
 @onready var timer_check_progress_timeout = $Timer_CheckProgressTimeout
 @onready var debug_chronometer := Chronometer.new()
+@onready var texture_progress_bar: TextureProgressBar = %TextureProgressBar
 
 static var _low_spec_toast_shown: bool = false
 
 
 func _ready():
 	last_activity_time = Time.get_ticks_msec()
-	item_count = carousel.item_count()
-	set_item(randi_range(0, item_count - 1))
-
 
 # Forward
 func enable_loading_screen():
@@ -66,13 +51,11 @@ func async_hide_loading_screen_effect():
 	timer_check_progress_timeout.stop()
 	_low_spec_toast_shown = false
 	var tween = get_tree().create_tween()
-	background.use_parent_material = true  # disable material
 	modulate = Color.WHITE
 	tween.tween_property(self, "modulate", Color.TRANSPARENT, 1.0)
 	await tween.finished
 	hide()
 	modulate = Color.WHITE
-	background.use_parent_material = false  # enable material
 	self.position.y = 0
 
 	if Global.cli.measure_perf:
@@ -84,78 +67,14 @@ func async_hide_loading_screen_effect():
 		counter.log_active_counts()
 
 
-func _on_texture_rect_right_arrow_gui_input(event):
-	if event is InputEventScreenTouch:
-		if event.pressed:
-			next_page()
-
-
-func _on_texture_rect_left_arrow_gui_input(event):
-	if event is InputEventScreenTouch:
-		if event.pressed:
-			prev_page()
-
-
-func prev_page():
-	var next_index = item_index - 1
-	if next_index < 0:
-		next_index = item_count - 1
-
-	set_item(next_index, false)
-
-
-func next_page():
-	var next_index = item_index + 1
-	if next_index >= item_count:
-		next_index = 0
-
-	set_item(next_index, true)
-
-
-func set_item(index: int, right_direction: bool = true):
-	timer_auto_move_carousel.stop()
-	timer_auto_move_carousel.start()
-
-	var current_color = Color(bg_colors[item_index])
-	item_index = index
-	carousel.set_item(index, right_direction)
-
-	var new_color = Color(bg_colors[index])
-	set_bg_shader_color(current_color, new_color)
-
-
-func set_bg_shader_color(from: Color, to: Color):
-	# Bail if we've left the tree (e.g. sign-out freed the loading screen between
-	# frames): get_tree() would be null and create_tween() would crash.
-	if not is_inside_tree():
-		return
-	var tween = get_tree().create_tween()
-	tween.tween_method(set_shader_background_color, from, to, 0.25)
-
-
-func set_shader_background_color(color: Color):
-	var shader_material: ShaderMaterial = background.get_material()
-	shader_material.set_shader_parameter("lineColor", color)
-
-
 func set_progress(new_progress: float):
 	new_progress = clampf(new_progress, 0.0, 100.0)
 	if progress != new_progress:
 		last_activity_time = Time.get_ticks_msec()
 	progress = new_progress
 
-	loading_progress_label.text = "LOADING %d%%" % floor(progress)
-	# Bail if we've left the tree (e.g. sign-out freed the loading screen between
-	# frames): get_tree() would be null and create_tween() would crash.
-	if not is_inside_tree():
-		return
-	var tween = get_tree().create_tween()
-	var new_width = loading_progress.get_parent().size.x * (progress / 100.0)
-	tween.tween_property(loading_progress, "position:x", new_width, 0.1)
-
-
-func _on_timer_auto_move_carousel_timeout():
-	next_page()
+	loading_progress_label.text = "%d%%" % floor(progress)
+	texture_progress_bar.value = progress
 
 
 func _on_timer_check_progress_timeout_timeout():
@@ -244,8 +163,7 @@ func _show_low_spec_toast_if_needed():
 	toast.async_show()
 
 
-# For dev purposes
-func _on_color_rect_header_gui_input(event):
+func _on_texture_rect_logo_gui_input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch:
 		if event.pressed:
 			var elapsed_time = Time.get_ticks_msec() - last_hide_click
