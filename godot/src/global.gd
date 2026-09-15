633be4f2c8a562d4868b914e7ee88c4205ba3ade
Eibriel <eibriel@eibriel.com>
2026-09-15 10:47:35 -0300

    feat: Google Play in-app review prompt (Android) — #2739 (#2865)
    * adding app review prompt
    
    * review prompt debug panel
    
    * modal fix, debug panel improvement
    
    * remove review debug from settings
    
    * fix icon, set max text length

--- godot/src/global.gd
+++ godot/src/global.gd
@@ -122,16 +122,12 @@ var nft_fetcher: OpenSeaFetcher
 var nft_frame_loader: NftFrameStyleLoader
 
 var snapshot: Snapshot
-
 var music_player: MusicPlayer
-
 var preload_assets: PreloadAssets
-
 var locations: Node
-
 var modal_manager: ModalManager
-
 var upgrade_nudge_coordinator: UpgradeNudgeCoordinator
+var review_prompt_coordinator: ReviewPromptCoordinator
 
 var standalone = false
 
@@ -724,6 +720,9 @@ func _ready():
 	self.upgrade_nudge_coordinator = load("res://src/upgrade_nudge_coordinator.gd").new()
 	self.upgrade_nudge_coordinator.set_name("upgrade_nudge_coordinator")
 
+	self.review_prompt_coordinator = load("res://src/review_prompt_coordinator.gd").new()
+	self.review_prompt_coordinator.set_name("review_prompt_coordinator")
+
 	get_tree().root.add_child.call_deferred(self.cli)
 	get_tree().root.add_child.call_deferred(self.music_player)
 	get_tree().root.add_child.call_deferred(self.scene_fetcher)
@@ -731,6 +730,7 @@ func _ready():
 	get_tree().root.add_child.call_deferred(self.locations)
 	get_tree().root.add_child.call_deferred(self.modal_manager)
 	get_tree().root.add_child.call_deferred(self.upgrade_nudge_coordinator)
+	get_tree().root.add_child.call_deferred(self.review_prompt_coordinator)
 	get_tree().root.add_child.call_deferred(self.content_provider)
 	get_tree().root.add_child.call_deferred(self.scene_runner)
 	get_tree().root.add_child.call_deferred(self.realm)
